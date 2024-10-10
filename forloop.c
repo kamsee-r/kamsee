8124d6f4785f13bb07e84efc1120ae54e787b669
kamsee-r <kamseeriss@gmail.com>
2024-10-10 12:07:22 +0300

    Add files via upload

---
+++ forloop.c
@@ -0,0 +1,15 @@
+//for loop
+/*
+Author:Chris Munene Murithi
+Reg; CT101/G/21909/24
+*/
+#include<stdio.h>
+int main(){
+	int i=1;
+	
+do{
+	printf("%d\n",i);
+	i+=3;
+	}while(i<=100);
+	return 0;
+}
