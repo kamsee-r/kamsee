4c265cb61a57cfeb8f58fd69d2c3c7ad100725e3
kamsee-r <kamseeriss@gmail.com>
2024-10-10 18:34:07 +0300

    Add files via upload

---
+++ whileloop.c
@@ -0,0 +1,14 @@
+// while loop
+/*
+Author:Chris Munene Murithi
+Reg; CT101/G/21909/24
+*/
+#include<stdio.h>
+int main(){
+	int i=1;
+	while(i<=100){
+		printf("%d\n",i);
+		i+=3;
+	}
+	return 0;
+}
